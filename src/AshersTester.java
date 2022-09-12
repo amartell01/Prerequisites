f7ff387c1557e628aea322b34278a9e418d43351
Asher <asher.rossen@gmail.com>
2022-09-12 14:22:43 -0700

    creating test.txt File in Test Case

--- src/AshersTester.java
+++ src/AshersTester.java
@@ -1,5 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -8,6 +14,13 @@ class AshersTester {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		Path p = Paths.get("test.txt");
+        try {
+            Files.writeString(p, "example", StandardCharsets.ISO_8859_1);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 	}
 
 	@AfterAll
